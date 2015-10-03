9f269ca5c0e98526cdde813095daaeeeb00a47bd
Dario Caruso <darioyzf@gmail.com>
2015-10-03 06:33:01 +0200

    ok

---
+++ index.js
@@ -0,0 +1,52 @@
+var verb=require('verbo'),
+PouchDB=require('pouchdb');
+
+
+module.exports = function(from,to,callback) {
+
+  if (typeof variable === 'undefined') {
+replicationprocess=false
+
+  }
+  var dbfrom=new PouchDB(from);
+  var toremote=new PouchDB(to);
+  dbfrom.allDocs().then(function(docs){
+
+  if (!replicationprocess && docs.total_rows && docs.total_rows > 0){
+
+verb("replication start","debug","Pushtocouch");
+
+    replicationprocess=true;
+    dbfrom.replicate.to(toremote, {
+      retry: true
+    }).then(function(){
+      verb("replication","debug","Pushtocouch")
+
+      dbfrom.destroy().then(function(){
+
+        replicationprocess=false;
+        verb("replication process ended","info","Pushtocouch")
+if(callback){
+  callback()
+}
+
+
+      }).catch(function(err){
+        replicationprocess=false;
+        verb(err,"error","Pushtocouch")
+
+      });
+
+    }).catch(function(err){
+      replicationprocess=false;
+      verb("replication crash","error","Pushtocouch")
+
+    });
+
+
+  }
+}).catch(function(err){
+  verb('error on get all documents','error','localdb')
+})
+
+}
---
+++ tests/main.js
@@ -0,0 +1,26 @@
+var pushtocouchdb=require('../index.js'),
+verb=require('verbo'),
+rm=require('rm-r'),
+PouchDB=require('pouchdb');
+
+var db=new PouchDB('aa');
+db.put({_id:"test"}).then(function(){
+
+  pushtocouchdb('aa','bb',function(){
+    var dbb=new PouchDB('bb');
+dbb.get("test").then(function(){
+  verb("ok","info","replicated")
+
+  rm.dir('./bb')
+}).catch(function(err){
+  verb(err,"error","booh")
+});
+
+
+
+  }).catch(function(err){
+    verb(err,"error","booh")
+    rm.dir('../aa')
+  });
+
+})
