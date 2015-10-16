a824ff1a3bfeacb299fde25551b9d86303ea6701
Dario Caruso <darioyzf@gmail.com>
2015-10-16 12:15:02 +0200

    oki

--- tests/main.js
+++ tests/main.js
@@ -20,11 +20,8 @@ dbb.get("test").then(function(){
 
 
   }).catch(function(err){
-<<<<<<< HEAD
     verb(err,"error","booh2")
-=======
-    verb(err,"error","booh")
->>>>>>> d206dc2802b60a0d7cb8f3d97018bff67cb54dee
+
     rm.dir('./aa')
   });
 
